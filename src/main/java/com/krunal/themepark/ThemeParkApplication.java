326a39e48f9782f34286c6bf993326d9320ee2b2
Krunal Priyadarshi <KrunalPriyadarshi@carfax.com>
2025-12-23 12:22:42 -0500

    docker deployment

--- src/main/java/com/krunal/themepark/ThemeParkApplication.java
+++ src/main/java/com/krunal/themepark/ThemeParkApplication.java
@@ -2,10 +2,12 @@ package com.krunal.themepark;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ThemeParkApplication {
+public class ThemeParkApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
+        System.out.println(RideStatusService.getRideStatus("teacup"));
         SpringApplication.run(ThemeParkApplication.class);
     }
 }
